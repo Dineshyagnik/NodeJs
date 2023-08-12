48b0539ca9fd2e6e0eaf263edc93d459fb9a1a97
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-08-12 18:16:35 +0530

    simple api and file system form input

---
+++ api.js
@@ -0,0 +1,7 @@
+const data=[
+    {name:'Dinesh Yagnik',email:'dyyayavar7491@gmail.com',age:20},
+    {nme:'John Doe',email:'johndoe@gmail.com',age:23},
+    {nme:'Sam de',email:'samde@gmail.com',age:26}
+]
+
+module.exports=data;
