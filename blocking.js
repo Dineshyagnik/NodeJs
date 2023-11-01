941fc725b1f27250ccd01a386fda2fd40f215aeb
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-11-01 17:36:26 +0530

    fs

--- blocking.js
+++ blocking.js
@@ -1,4 +1,8 @@
 var fs = require('fs');
-var data=fs.readFileSync('apple.txt',);
-console.log(data.toString());
+var data1=fs.readFileSync('apple.txt',);
+var data2=fs.readFileSync('text.txt',);
+var data3=fs.readFileSync('text2.txt',);
+console.log(data1.toString());
+console.log(data2.toString());
+console.log(data3.toString());
 console.log('End here...');
