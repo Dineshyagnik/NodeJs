ad65cdda3640a4a5437820dc24967bfb5e2b99c5
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-11-08 19:47:08 +0530

    local db

--- expressapp.js
+++ expressapp.js
@@ -24,3 +24,8 @@ var bodyParser = require('body-parser');
 var json = require('json');
 var logger = require('logger');
 var methodOverride = require('method-override');
+
+var nano = require('nano')('http://localhost:5948');
+
+var db = nano.use('address');
+var app = express;
