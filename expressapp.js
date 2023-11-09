a3f8d82212beb3bd97449d6533ad5da5cdeb6b57
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-11-09 20:13:42 +0530

    new app

--- expressapp.js
+++ expressapp.js
@@ -1,31 +1,16 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
+const express = require('express')
+const app = express()
+const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// });
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
 
-// app.get('/about', (req, res) => {
-//   res.send('About Page!')
-// });
+app.get('/about', (req, res) => {
+  res.send('About Page!')
+});
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
 
-
-var express = require('express');
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
-var urlencoded = require('url');
-var bodyParser = require('body-parser');
-var json = require('json');
-var logger = require('logger');
-var methodOverride = require('method-override');
-
-var nano = require('nano')('http://localhost:5948');
-
-var db = nano.use('address');
-var app = express;
