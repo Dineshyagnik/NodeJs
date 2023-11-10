ca3e656b11ed421df5fefe26d2335f9d4ee5b7e0
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-11-10 22:58:00 +0530

    data

--- api.js
+++ api.js
@@ -1,7 +1,9 @@
 const data=[
     {name:'Dinesh Yagnik',email:'dyyayavar7491@gmail.com',age:20},
-    {nme:'John Doe',email:'johndoe@gmail.com',age:23},
-    {nme:'Sam de',email:'samde@gmail.com',age:26}
+    {name:'John Doe',email:'johndoe@gmail.com',age:23},
+    {name:'Sam de',email:'samde@gmail.com',age:26},
+    {name:'ani',email:'ani115@gmail.com',age:20},
+    {name:'menuka',email:'menuka125@gmail.com',age:19},
 ]
 
 module.exports=data;
