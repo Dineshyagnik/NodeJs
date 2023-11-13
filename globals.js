149224822c9682284aacdfa3075b9dd49b4a005b
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-11-13 20:45:26 +0530

    global signal

--- globals.js
+++ globals.js
@@ -7,12 +7,17 @@
 
 // console.log(ac.signal.aborted); 
 
+// const ac = new AbortController();
+
+// ac.signal.onabort = () => console.log('aborted!');
+
+// ac.signal.addEventListener('abort', (event) => {
+//     console.log(event.type);
+// }, { once: true });
+
+// ac.abort(); 
+
+
 const ac = new AbortController();
-
-ac.signal.onabort = () => console.log('aborted!');
-
-ac.signal.addEventListener('abort', (event) => {
-    console.log(event.type);
-}, { once: true });
-
-ac.abort(); 
+ac.abort(new Error('boom!'));
+console.log(ac.signal.reason);  // Error: boom! 
