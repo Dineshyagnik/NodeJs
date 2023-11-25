2b55a7fed8f3db4d1055d4748f3896cfc8de3c9c
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-11-25 18:32:12 +0530

    http server

--- httpserver.js
+++ httpserver.js
@@ -1,14 +1,28 @@
-const http = require('http');
+// const http = require('http');
 
-const port = process.env.PORT || 4000;
+// const port = process.env.PORT || 4000;
 
-const sever = http.createServer((req, res) => {
-    console.log(req.url);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Nodejs Learning </h1> <p>Hello World </p>');
-});
+// const sever = http.createServer((req, res) => {
+//     console.log(req.url);
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html')
+//     res.end('<h1>Nodejs Learning </h1> <p>Hello World </p>');
+// });
 
-sever.listen(port, () => {
-    console.log(`Server is listening on ${port}`);
-});
+// sever.listen(port, () => {
+//     console.log(`Server is listening on ${port}`);
+// });
+
+const http = require('node:http');
+// const keepAliveAgent = new http.Agent({ keepAlive: true });
+// options.agent = keepAliveAgent;
+// http.request(options, onResponseCallback);
+
+http.get({
+    hostname: 'localhost',
+    port: 80,
+    path: '/',
+    agent: false,  // Create a new agent just for this one request
+  }, (res) => {
+    // Do stuff with response
+  }); 
