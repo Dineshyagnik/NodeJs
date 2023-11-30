c956b371ca3946b539eb3b1b371c4f0b35cb8b30
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-11-30 19:23:26 +0530

    http2

--- httpserver.js
+++ httpserver.js
@@ -13,16 +13,37 @@
 //     console.log(`Server is listening on ${port}`);
 // });
 
-const http = require('node:http');
-// const keepAliveAgent = new http.Agent({ keepAlive: true });
-// options.agent = keepAliveAgent;
-// http.request(options, onResponseCallback);
+// const http = require('node:http');
+// // const keepAliveAgent = new http.Agent({ keepAlive: true });
+// // options.agent = keepAliveAgent;
+// // http.request(options, onResponseCallback);
 
-http.get({
-    hostname: 'localhost',
-    port: 80,
-    path: '/',
-    agent: false,  // Create a new agent just for this one request
-  }, (res) => {
-    // Do stuff with response
-  }); 
+// http.get({
+//     hostname: 'localhost',
+//     port: 80,
+//     path: '/',
+//     agent: false,  // Create a new agent just for this one request
+//   }, (res) => {
+//     // Do stuff with response
+//   }); 
+
+
+const http2 = require('node:http2');
+const fs = require('node:fs');
+
+const server = http2.createSecureServer({
+  key: fs.readFileSync('localhost-privkey.pem'),
+  cert: fs.readFileSync('localhost-cert.pem'),
+});
+server.on('error', (err) => console.error(err));
+
+server.on('stream', (stream, headers) => {
+  // stream is a Duplex
+  stream.respond({
+    'content-type': 'text/html; charset=utf-8',
+    ':status': 200,
+  });
+  stream.end('<h1>Hello World</h1>');
+});
+
+server.listen(8443); 
