9d556880c62721c3eec21007f6865f77b6a4a977
Dinesh Kumar <101325099+Dineshyagnik@users.noreply.github.com>
2024-06-06 18:06:08 +0530

    Events

--- index.js
+++ index.js
@@ -598,16 +598,38 @@
 // console.log(process.argv[2]);
 
 
-const fs = require('fs');
+// const fs = require('fs');
 
-const input = process.argv;
+// const input = process.argv;
 
-if(input[2]=='add'){
-    fs.writeFileSync(input[3],input[4]);
-}else if(input[2]=='remove'){
-    fs.unlinkSync(input[3]);
-}
-else{
-    console.log('invailid input');
-}
+// if(input[2]=='add'){
+//     fs.writeFileSync(input[3],input[4]);
+// }else if(input[2]=='remove'){
+//     fs.unlinkSync(input[3]);
+// }
+// else{
+//     console.log('invailid input');
+// }
 
+
+
+// const EventEmitter = require('node:events');
+
+// class MyEmitter extends EventEmitter {}
+
+// const myEmitter = new MyEmitter();
+// myEmitter.on('event', () => {
+//   console.log('an event occurred!');
+// });
+// myEmitter.emit('event');
+
+
+
+const EventEmitter = require('node:events');
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+myEmitter.on('event', (a, b) => {
+  console.log(a, b, this);
+  // Prints: a b {}
+});
+myEmitter.emit('event', 'a', 'b');
