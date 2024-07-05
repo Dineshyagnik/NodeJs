95fd03855ec7fe3078215eb8c6baf2289171ea18
Dinesh Kumar <101325099+Dineshyagnik@users.noreply.github.com>
2024-07-05 17:59:22 +0530

    making page

--- index.js
+++ index.js
@@ -838,6 +838,59 @@
 // app.listen(4200);
 
 
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// const publicPath = path.join(__dirname,'public');
+
+// // app.use(express.static(publicPath));
+
+// app.get('',(_,resp)=>{
+//   resp.sendFile(`${publicPath}/index.html`)
+// });
+
+// app.get('/about',(_,resp)=>{
+//   resp.sendFile(`${publicPath}/about.html`)
+// });
+
+// app.get('/help',(_,resp)=>{
+//   resp.sendFile(`${publicPath}/help.html`)
+// });
+
+// app.get('*',(_,resp)=>{
+//   resp.sendFile(`${publicPath}/nopage.html`)
+// });
+
+// app.listen(4200);
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// const publicPath = path.join(__dirname,'public');
+
+// // app.use(express.static(publicPath));
+
+// app.get('',(_,resp)=>{
+//   resp.sendFile(`${publicPath}/index.html`)
+// });
+
+// app.get('/about',(_,resp)=>{
+//   resp.sendFile(`${publicPath}/about.html`)
+// });
+
+// app.get('/help',(_,resp)=>{
+//   resp.sendFile(`${publicPath}/help.html`)
+// });
+
+// app.get('*',(_,resp)=>{
+//   resp.sendFile(`${publicPath}/nopage.html`)
+// });
+
+// app.listen(4200);
+
+
 const express = require('express');
 const path = require('path');
 const app = express();
